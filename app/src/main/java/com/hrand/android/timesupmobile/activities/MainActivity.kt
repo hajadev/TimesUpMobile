fce377d118d8d41f7e802238300f1864a1f02505
Haja <haja.dev@gmail.com>
2018-12-12 16:04:59 +0100

    Cleaning

--- app/src/main/java/com/hrand/android/timesupmobile/activities/MainActivity.kt
+++ app/src/main/java/com/hrand/android/timesupmobile/activities/MainActivity.kt
@@ -3,17 +3,13 @@ package com.hrand.android.timesupmobile.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.Toast
 import com.hrand.android.timesupmobile.R
 import com.hrand.android.timesupmobile.daos.WordDao
 import com.hrand.android.timesupmobile.models.Theme
-import com.hrand.android.timesupmobile.models.Theme_
-import com.hrand.android.timesupmobile.models.Word
 import com.hrand.android.timesupmobile.utils.*
 import io.objectbox.Box
 import io.objectbox.kotlin.boxFor
-import io.objectbox.kotlin.query
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -46,36 +42,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initDataBase(){
-        //ObjectBox.build(this)
+
         themesBox = ObjectBox.boxStore.boxFor()
-        //wordsBox = ObjectBox.boxStore.boxFor()
 
         val themes = initThemesData()
-        /*Log.i("haja", "listes des themes (${themes.size}) a insérer")
-        for (t in themes){
-            Log.i("haja", t.value)
-        }*/
         themesBox.put(themes)
-        //wordsBox.put(initWordsData())
         WordDao.init(this)
 
         val listToInsert = initWordsData()
-        Log.i("haja", "listes des mots (${listToInsert.size}) à insérer")
-        for (w in listToInsert ){
-            Log.i("haja", "${w.value}")
-        }
         WordDao.addWords(listToInsert)
-        val listeW: List<Word> = WordDao.getAll()
-        Log.i("haja", "listes des mots (${listeW.size}) en base")
-        for (w in listeW ){
-            Log.i("haja", "${w.id} / ${w.value}")
-        }
-
-        val listeTheme = themesBox.query{order(Theme_.id)}.find()
-        /*Log.i("haja", "listes des themes (${listeTheme.size}) en base")
-        for (t in listeTheme){
-            Log.i("haja", t.value)
-        }*/
 
         Toast.makeText(this, "Données chargés", Toast.LENGTH_SHORT).show()
     }
