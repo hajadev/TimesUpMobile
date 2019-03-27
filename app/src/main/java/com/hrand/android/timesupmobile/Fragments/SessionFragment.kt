657205ea73709053ab2a8cd0b8a632293389fb1a
Haja <haja.dev@gmail.com>
2019-03-27 12:12:23 +0100

    Fragment to display word and timer

---
+++ app/src/main/java/com/hrand/android/timesupmobile/Fragments/SessionFragment.kt
@@ -0,0 +1,27 @@
+package com.hrand.android.timesupmobile.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.hrand.android.timesupmobile.R
+import com.hrand.android.timesupmobile.activities.GameActivity
+
+class SessionFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val currentActivity = this.activity as GameActivity
+        //super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fragment_session, container, false)
+
+        return view
+    }
+
+    companion object {
+
+        fun newInstance(): SessionFragment {
+            return SessionFragment()
+        }
+    }
+}
