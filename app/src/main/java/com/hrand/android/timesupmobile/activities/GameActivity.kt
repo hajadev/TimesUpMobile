933a5ce18c584dac140a9451c1ffe502542ac6c8
Haja <haja.dev@gmail.com>
2019-03-27 11:06:09 +0100

    Function to get shuffled list

--- app/src/main/java/com/hrand/android/timesupmobile/activities/GameActivity.kt
+++ app/src/main/java/com/hrand/android/timesupmobile/activities/GameActivity.kt
@@ -3,6 +3,8 @@ package com.hrand.android.timesupmobile.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.hrand.android.timesupmobile.R
+import com.hrand.android.timesupmobile.daos.WordDao
+import com.hrand.android.timesupmobile.models.Word
 
 class GameActivity : AppCompatActivity() {
 
@@ -11,6 +13,8 @@ class GameActivity : AppCompatActivity() {
     var nbTeam = 2
     var currentSession = 1
     var currentTeam = 1
+
+    lateinit var wordsList: List<Word>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +26,21 @@ class GameActivity : AppCompatActivity() {
             nbTeam = savedInstanceState.getInt("nbTeam", 2)
         }
 
+        wordsList = initWords(8)
+
     }
 
+    private fun initWords(nbWord: Int) : List<Word> {
+        val listToReturn = ArrayList<Word>()
+        val allWords  = WordDao.getAll()
+
+        allWords.shuffled()
+        for(i in 0..nbWord){
+            listToReturn.add(allWords[i])
+        }
+
+        return listToReturn
+    }
 
 
 }
