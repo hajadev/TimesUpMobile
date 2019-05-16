756405d99e27264b7e5f12f68a2e53e6a47fd5d3
Haja <haja.dev@gmail.com>
2019-05-16 12:01:22 +0200

    Graphical refactoring

--- app/src/main/java/com/hrand/android/timesupmobile/fragments/StatFragment.kt
+++ app/src/main/java/com/hrand/android/timesupmobile/fragments/StatFragment.kt
@@ -20,10 +20,12 @@ import nl.dionsegijn.konfetti.models.Size
 class StatFragment : Fragment() {
 
     lateinit var gameActivity: GameActivity
+    var widthGlobal = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         gameActivity = this.activity as GameActivity
         val view = inflater.inflate(R.layout.fragment_stat, container, false)
+        widthGlobal = view.width
 
         return view
     }
@@ -38,6 +40,7 @@ class StatFragment : Fragment() {
         super.onStart()
         tv_winner.text = "L'équipe ${gameActivity.winner} a gagné !!!"
         setListener()
+        animate()
         viewKonfetti.build()
                 .addColors(Color.YELLOW, Color.LTGRAY/*, Color.GREEN, Color.MAGENTA*/)
                 .setDirection(0.0, 359.0)
@@ -46,14 +49,8 @@ class StatFragment : Fragment() {
                 .setTimeToLive(2000L)
                 .addShapes(Shape.RECT, Shape.CIRCLE)
                 .addSizes(Size(12))
-                .setPosition(-50f, viewKonfetti.width + 50f, -50f, -50f)
+                .setPosition(0f, rl_stat_fragment.width + 50f, -50f, -50f)
                 .streamFor(100, 5000L)
-        animate()
-        /*
-        animate1()
-        animate2()
-        animate3()
-        */
     }
 
     private fun animate(){
@@ -65,37 +62,6 @@ class StatFragment : Fragment() {
         star_2.startAnimation(animation2)
         animation3.startOffset=1000
         star_3.startAnimation(animation3)
-    }
-
-    private fun animate1(){
-        //UI thread
-        Thread(Runnable {
-            gameActivity.runOnUiThread(java.lang.Runnable {
-                val animation1 = AnimationUtils.loadAnimation(gameActivity, R.anim.my_animation)
-                star_1.startAnimation(animation1)
-                Thread.sleep(1000)
-            })
-        }).start()
-    }
-    private fun animate2(){
-        //UI thread
-        Thread(Runnable {
-            gameActivity.runOnUiThread(java.lang.Runnable {
-                val animation1 = AnimationUtils.loadAnimation(gameActivity, R.anim.my_animation)
-                star_2.startAnimation(animation1)
-                Thread.sleep(1000)
-            })
-        }).start()
-    }
-    private fun animate3(){
-        //UI thread
-        Thread(Runnable {
-            gameActivity.runOnUiThread(java.lang.Runnable {
-                val animation1 = AnimationUtils.loadAnimation(gameActivity, R.anim.my_animation)
-                star_3.startAnimation(animation1)
-                Thread.sleep(1000)
-            })
-        }).start()
     }
 
     companion object {
